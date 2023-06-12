3567207971e304206be96a91c524d72fe94f77b9
Dagohe89 <gotokrm89@gmail.com>
2023-06-13 01:08:52 +0200

    130623

--- public/js/calendario.js
+++ public/js/calendario.js
@@ -1,16 +1,36 @@
-/*const equipos =  [ { num: 0, local: true, ronda: false },
-  { num: 1, local: false, ronda: false },
-  { num: 2, local: true, ronda: false },
-  { num: 3, local: false, ronda: false },
-  { num: 4, local: true, ronda: false },
-  { num: 5, local: false, ronda: false },
-  { num: 6, local: true, ronda: false },
-  { num: 7, local: false, ronda: false },
-  { num: 8, local: true, ronda: false },
-  { num: 9, local: false, ronda: false },
+/*const equipos =  [ { nombre: "A", local: true, ronda: false, duelos: ["A"] },
+  { nombre: "B", local: false, ronda: false, duelos: ["B"]},
+  { nombre: "C", local: true, ronda: false, duelos: ["C"] },
+  { nombre: "D", local: false, ronda: false, duelos: ["D"] },
+  { nombre: "E", local: true, ronda: false, duelos: ["E"] },
+  { nombre: "F", local: false, ronda: false, duelos: ["F"] },
+  { nombre: "G", local: true, ronda: false, duelos: ["G"] },
+  { nombre: "H", local: false, ronda: false, duelos: ["H"] },
+  { nombre: "I", local: true, ronda: false, duelos: ["I"] },
+  { nombre: "J", local: false, ronda: false, duelos: ["J"] },
 ];
 
-for (let j = 1; j <= 9; j++) {
+for (let j = 1; j <=9; j++) {
+  console.log("Ronda: " + j + " -------");
+  let i = 0;
+  while (equipos[i].ronda == false) {
+    let local = [];
+    let visitante = [];
+    if (equipos[i].local) {
+      local[i] = equipos[i].nombre;
+      equipos[i].local = false;
+      equipos[i].ronda = true;
+    } else {
+      visitante[i] = equipos[i].nombre;
+      equipos[i].local = true;
+      equipos[i].ronda = true;
+    }
+    console.log(local[i] + "vs" + visitante[i]);
+    i++;
+  }
+}*/
+
+/*for (let j = 1; j <= 9; j++) {
     console.log("Ronda: " + j + " ------");
       let local = equipos[0];
       let visitante = equipos[1];
