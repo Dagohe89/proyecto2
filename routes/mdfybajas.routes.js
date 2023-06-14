28d2a3f80b58dde57d7db16fc93af16668849115
Dagohe89 <gotokrm89@gmail.com>
2023-06-14 13:21:02 +0200

    140623

--- routes/mdfybajas.routes.js
+++ routes/mdfybajas.routes.js
@@ -5,45 +5,5 @@ const router = express.Router();
 const db_connection = require('../database/connection.js');
 const bcrypt = require('bcrypt');
 
-router.post('/mdfybajadelegado', function (req, res) {
-    // Recoger los datos del formulario de texto
-    const campoActualizar = req.body.campoActualizar;
-    const password = req.body.password;
 
-    // Recoger el campo de archivo (si lo hay)
-    const archivo = req.files && req.files.archivo;
-
-    // Realizar acciones según sea necesario con los campos de texto
-    // ...
-
-    // Comprobar si es un campo de contraseña y verificarla con bcrypt
-    if (password) {
-        const hashedPassword = '$2b$10$YourHashedPassword'; // Obtén la contraseña hasheada almacenada en tu base de datos
-        const passwordMatch = bcrypt.compareSync(password, hashedPassword);
-
-        if (passwordMatch) {
-            // La contraseña coincide
-            // Realizar acciones adicionales según sea necesario
-        } else {
-            // La contraseña no coincide
-            // Realizar acciones adicionales según sea necesario
-        }
-    }
-
-    // Realizar acciones según sea necesario con el campo de archivo
-    if (archivo) {
-        // Procesar el archivo adjunto
-        // ...
-    }
-
-    // Enviar una respuesta al cliente
-    res.send('¡Formulario enviado exitosamente!');
-});
-
-router.post('/mdfybajasequipo', (req, res) => {
-
-});
-
-router.post('/mdfybajasjugador', (req, res) => {
-
-});
+module.exports = router;
