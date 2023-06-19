41a6ebc27f45b28330b60179b1377eb9dcd19aca
Dagohe89 <gotokrm89@gmail.com>
2023-06-19 19:03:37 +0200

    190623modificacionloginfetch

--- public/js/header.js
+++ public/js/header.js
@@ -87,62 +87,31 @@ form.addEventListener('submit', (e) => {
   e.preventDefault();
 })
 
-// Obtener la pestaña que deseas resaltar
-/*document.addEventListener('DOMContentLoaded', function() {
-  // Obtener la pestaña actual del enlace
-  const currentTab = window.location.pathname;
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+  event.preventDefault();
 
-  // Obtener todos los elementos de la lista de navegación
-  const navItems = document.querySelectorAll('.navbar-nav .nav-item');
+  // Obtén los valores de los campos de usuario y contraseña
+  var username = document.getElementById('username').value;
+  var password = document.getElementById('password').value;
 
-  // Recorrer los elementos de la lista de navegación
-  navItems.forEach((item) => {
-    // Obtener el enlace dentro del elemento de la lista
-    const link = item.querySelector('a');
-
-    // Obtener el valor del atributo "href" del enlace
-    const href = link.getAttribute('href');
-
-    // Verificar la pestaña actual usando un switch
-    switch (currentTab) {
-      case "/":
-        if (href === "/") {
-          link.classList.add('active');
-        }
-        break;
-      case "/competicion":
-        if (href === "/competicion") {
-          link.classList.add('active');
-        }
-        break;
-      case "/equipos":
-        if (href === "/equipos") {
-          link.classList.add('active');
-        }
-        break;
-      case "/actualidad":
-        if (href === "/actualidad") {
-          link.classList.add('active');
-        }
-        break;
-      case "/galeria":
-        if (href === "/galeria") {
-          link.classList.add('active');
-        }
-        break;
-      case "/inscripciones":
-        if (href === "/inscripciones") {
-          link.classList.add('active');
-        }
-        break;
-      case "/miequipo":
-        if (href === "/miequipo") {
-          link.classList.add('active');
-        }
-        break;
-      default:
-        // No se encontró ninguna coincidencia
-        break;
-    }
-  });
-});*/
+  // Realiza una solicitud AJAX utilizando fetch
+  fetch('/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ username: username, password: password })
+  })
+    .then(function(response) {
+      if (response.ok) {
+        // Inicio de sesión exitoso, redirige al usuario a otra página
+        window.location.href = '/dashboard';
+      } else {
+        // Mostrar mensaje de error si falla el inicio de sesión
+        alert('Error al iniciar sesión. Por favor, verifica tus credenciales.');
+      }
+    })
+    .catch(function(error) {
+      console.error('Error de red:', error);
+    });
+});
